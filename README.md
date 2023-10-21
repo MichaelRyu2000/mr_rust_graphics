5629fdebaee9f9361edb40f44660119652b0be88
michael-ryu-1 <ryumichael@g.ucla.edu>
2023-10-21 15:23:23 -0700

    Changed tutorials to one linked in README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mr_rust_graphics
 Following a tutorial on wgpu
+
+Tutorial Link: https://sotrh.github.io/learn-wgpu/
+
+
+
--- wgpu02/src/main.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let instances = wgpu::Instance::new(wgpu::InstanceDescriptor {
-        backends: wgpu::Backends::all(),
-        dx12_shader_compiler: wgpu::Dx12Compiler::default(),
-    });
-    for adapter in instances.enumerate_adapters(wgpu::Backends::all()) {
-        println!("{:?}", adapter.get_info())
-    }
-}
